bbe0790e72b635ee22af3ba01264d1512dc0588d
Romain ROGUET <romain.roguet@gmail.com>
2022-10-03 11:24:30 +0200

    main scss ok

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,25 +1,27 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Home from "./pages/Home";
+import Profile from "./pages/Profile";
+import Connexion from "./pages/Connexion";
+import Header from "./components/Header";
+import EditProfile from "./pages/EditProfile";
+import AxiosInterceptor from "./components/AxiosInterceptor";
 
-function App() {
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+      <AxiosInterceptor>
+        <Header />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/profile" element={<Profile />} />
+          <Route path="/edit-profile" element={<EditProfile />} />
+          <Route path="*" element={<Home />} />
+          <Route path="/connexion" element={<Connexion />} />
+        </Routes>
+      </AxiosInterceptor>
+    </BrowserRouter>
   );
-}
+};
 
 export default App;
