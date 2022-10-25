ea3a759bf6789f86c0baa11335e8af9ee6907cb7
Romain ROGUET <romain.roguet@gmail.com>
2022-10-25 20:22:41 +0200

    user model

--- backend/middleware/auth.js
+++ backend/middleware/auth.js
@@ -1,5 +1,5 @@
 const jwt = require('jsonwebtoken');
-const UserModel = require('../models/user.model');
+const UserModel = require('../models/user');
 
 //vérification de l'utilisateur et du token pour accès
 module.exports.checkUser = (req, res, next) => {
