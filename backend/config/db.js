76b170c91e92ce46e9e1a127481390e926ddb19b
Romain ROGUET <romain.roguet@gmail.com>
2022-10-25 20:43:00 +0200

    db .env

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,12 +1,11 @@
 const mongoose = require("mongoose");
 
+// définit l'accès à la BDD MongoDB (utilise les var d'env pour ne pas transmettre les logs de connexion en clair dans le code)
+
 mongoose
   .connect(
-    process.env.DB_USER_PASS,
-    {
-      useNewUrlParser: true,
-      useUnifiedTopology: true
-    }
+    'mongodb+srv://UserP7:CalimerO01@groupomania.fpzzvg1.mongodb.net/groupomania',
+    { useNewUrlParser: true, useUnifiedTopology: true }
   )
-  .then(() => console.log("Connected to MongoDB"))
-  .catch((err) => console.log("Failed to connect to MongoDB", err));
+  .then(() => console.log("Connexion à MongoDB réussie !"))
+  .catch(() => console.log("Connexion à MongoDB échouée !"));
